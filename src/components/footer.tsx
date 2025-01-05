ed3447890c29f1e2bf20e095cf04e4617161ab4e
larads <dasilval.mariana@gmail.com>
2025-01-04 22:14:07 -0400

    feat: creating the footer component for copyright

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <div className="fixed bottom-0 left-0 right-0 py-2 bg-gray-800">
+        <div className="flex justify-center items-center">
+          <p className="text-gray-400 text-sm">© 2024 Galeria de Fotos. Todos os direitos reservados.</p>
+        </div>
+      </div>
+    );
+  }
+  
