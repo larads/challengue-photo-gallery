389a91fcb75bd4f8aef95e44a7aabdf317148bb2
larads <dasilval.mariana@gmail.com>
2025-01-04 21:31:52 -0400

    feat: adjusting tailwindcss and creating image utils

---
+++ src/utils/imagens.ts
@@ -0,0 +1,17 @@
+export const images = [
+  { id: 1, src: "https://aventurasnahistoria.com.br/media/uploads/hard_news/lontra_capa.jpg", title: "Lontra", data: new Date().toISOString() },
+  { id: 2, src: "https://www.turismocaragua.com.br/wp-content/uploads/2023/06/Fotos-lontra-mar-kelp-ecossistema.webp", title: "Imagem 2", data: new Date(new Date().setDate(new Date().getDate() - 3)).toISOString() },
+  { id: 3, src: "https://mega.ibxk.com.br/2021/03/24/24093417684033.jpg?ims=fit-in/800x500", title: "Imagem 3", data: new Date(new Date().setDate(new Date().getDate() - 5)).toISOString() },
+  { id: 4, src: "https://faunanews.com.br/wp-content/uploads/2022/02/image-19.png", title: "Imagem 4", data: new Date(new Date().setDate(new Date().getDate() - 7)).toISOString() },
+  { id: 5, src: "https://anfitrioesdealuguel.com.br/wp-content/uploads/2019/09/original-3d62e1d839e423af772687060a8748c5.jpg", title: "Imagem 5", data: new Date().toISOString() },
+  { id: 6, src: "https://segredosdomundo.r7.com/wp-content/uploads/2019/11/lontra-caracteristicas-como-se-reproduz-e-onde-vive-5.jpg", title: "Imagem 6", data: new Date(new Date().setDate(new Date().getDate() - 3)).toISOString() },
+  { id: 7, src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC6p3IWAxi8jvxB4lwWuJs3oOGIuVGyPYqng&s", title: "Imagem 7", data: new Date(new Date().setDate(new Date().getDate() - 5)).toISOString() },
+  { id: 8, src: "https://qcanimais.com.br/wp-content/uploads/2019/12/Lontra-2.jpg", title: "Imagem 8", data: new Date(new Date().setDate(new Date().getDate() - 7)).toISOString() },
+  { id: 9, src: "https://s3.animalia.bio/animals/photos/full/1.25x1/a-pair-of-european-otters-close-up.webp?id=e1078571f116a1d2cf8a92ebd525d338", title: "Imagem 9", data: new Date().toISOString() },
+  { id: 10, src: "https://www.publicdomainpictures.net/pictures/30000/nahled/otters.jpg", title: "Imagem 10", data: new Date(new Date().setDate(new Date().getDate() - 3)).toISOString() },
+  { id: 11, src: "https://conexaoplaneta.com.br/wp-content/uploads/2019/04/depois-22-anos-charlie-lontra-mais-antiga-mundo-morre-legado-historia-linda-conexao-planeta.jpg", title: "Imagem 11", data: new Date(new Date().setDate(new Date().getDate() - 5)).toISOString() },
+  { id: 12, src: "https://animalbusiness.com.br/wp-content/uploads/2020/07/Loutre_des_pyrenees.jpg", title: "Imagem 12", data: new Date(new Date().setDate(new Date().getDate() - 7)).toISOString() },
+  { id: 13, src: "https://cdn0.peritoanimal.com.br/pt/posts/9/3/9/as_lontras_sao_perigosas_24939_orig.jpg", title: "Imagem 13", data: new Date().toISOString() },
+  { id: 14, src: "https://www.publicdomainpictures.net/pictures/30000/nahled/otters.jpg", title: "Imagem 14", data: new Date(new Date().setDate(new Date().getDate() - 3)).toISOString() },
+  { id: 15, src: "https://www.publicdomainpictures.net/pictures/30000/nahled/otters.jpg", title: "Imagem 15", data: new Date(new Date().setDate(new Date().getDate() - 5)).toISOString() },
+];
